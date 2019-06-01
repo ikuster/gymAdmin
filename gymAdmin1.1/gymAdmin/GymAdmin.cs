d432f682b0bd1a81796a301786f649867f0eba6d
ikuster <ikuster@foi.hr>
2019-06-01 16:19:33 +0200

    Prve četiri funkcionalnosti
    Napravljena baza podataka, prijava u sustav, registracija zaposlenika(korisniku se prikazuje generirano korisnicko ime i lozinka, ali se u bazu sprema heširana lozinka zbog sigurnosti), prikaz svih zaposlenika, brisanje te pretrazivanje zaposlenika. Potrebno jos uređivanje i provjera kvalitete OOP-a

---
+++ gymAdmin1.1/gymAdmin/GymAdmin.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gymAdmin
+{
+    public partial class GymAdmin : Form
+    {
+       
+        public GymAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void TableLayout_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BtnPocetna_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelZaposlenici_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BtnZaposlenici_Click(object sender, EventArgs e)
+        {
+            PanelActive.Location = new System.Drawing.Point(174, 276);
+
+            Zaposlenici formaZaposlenici = new Zaposlenici();
+            formaZaposlenici.Show();
+        }
+
+        private void BtnKlijenti_Click(object sender, EventArgs e)
+        {
+            PanelKlijenti.BringToFront();
+            PanelActive.Location = new System.Drawing.Point(174, 69);
+        }
+
+        private void BtnPocetna_Click(object sender, EventArgs e)
+        {
+            PanelActive.Location = new System.Drawing.Point(174, 0);
+        }
+
+        private void BtnClanarine_Click(object sender, EventArgs e)
+        {
+            PanelActive.Location = new System.Drawing.Point(174, 138);
+        }
+
+        private void BtnMjerenja_Click(object sender, EventArgs e)
+        {
+            PanelActive.Location = new System.Drawing.Point(174, 207);
+        }
+
+        private void BtnDodajKlijenti_Click(object sender, EventArgs e)
+        {
+            NoviClan noviClan = new NoviClan();
+            noviClan.Show();
+        }
+
+        private void BtnIzbrisiKlijenti_Click(object sender, EventArgs e)
+        {
+            PotvrdaBrisanja formaBrisanje = new PotvrdaBrisanja();
+            formaBrisanje.Show();
+        }
+
+        private void BtnMjerenjaKlijenti_Click(object sender, EventArgs e)
+        {
+            Mjerenja formMjerenja = new Mjerenja();
+            formMjerenja.Show();
+        }
+
+        private void BtnDetaljnoKlijenti_Click(object sender, EventArgs e)
+        {
+            DetaljiKlijent formaDetaljiKlijent = new DetaljiKlijent();
+            formaDetaljiKlijent.Show();
+        }
+    }
+} 
---
+++ gymAdmin1.1/gymAdmin/ZaposlenikRadnje.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gymAdmin
+{
+    class ZaposlenikRadnje
+    {
+        private string GenerirajKorIme(Zaposlenik zaposlenik)
+        {
+            string korIme = zaposlenik.Ime[0] + zaposlenik.Prezime;
+            korIme = korIme.ToLower();
+            return korIme;
+        }
+        private string GenerirajLozinku()
+        {
+            string lozinka=System.Web.Security.Membership.GeneratePassword(7, 1);
+            return lozinka;
+
+        }
+        public int Autoriziraj(string korIme,string lozinka)
+        {
+            int provjeriPostojanje;
+            using (var baza = new Entities())
+            {
+                provjeriPostojanje = (from z in baza.Zaposlenik
+                                      where z.Korisnicko_ime == korIme
+                                      && z.Lozinka == lozinka
+                                      select z).Count();
+            }
+            return provjeriPostojanje;
+        }
+
+        public List<Zaposlenik> DohvatiSveIzBaze()
+        {
+            List<Zaposlenik> zaposlenici;
+            using(var baza = new Entities())
+            {
+                var upitDohvati = from z in baza.Zaposlenik
+                                  select z;
+                zaposlenici = upitDohvati.ToList();
+            }
+            return zaposlenici;
+
+        }
+        public void IzbrisiIzBaze(Zaposlenik odabraniZaposlenik)
+        {
+            using(var baza = new Entities())
+            {
+                baza.Zaposlenik.Attach(odabraniZaposlenik);
+                baza.Zaposlenik.Remove(odabraniZaposlenik);
+                baza.SaveChanges();
+            }
+        }
+        public List<string> KreirajKredencijale(Zaposlenik noviZaposlenik) {
+            List<string> kredencijali = new List<string>();
+            string korIme = GenerirajKorIme(noviZaposlenik);
+            string lozinka = GenerirajLozinku();
+            kredencijali.Add(korIme);
+            kredencijali.Add(lozinka);
+            return kredencijali;
+        }
+        public void DodajUBazu(Zaposlenik noviZaposlenik)
+        {
+
+            using(var baza = new Entities())
+            {
+                Zaposlenik zaposlenik = new Zaposlenik {
+                    Ime = noviZaposlenik.Ime,
+                    Prezime = noviZaposlenik.Prezime,
+                    Korisnicko_ime = noviZaposlenik.Korisnicko_ime,
+                    Lozinka = GenerirajHash(noviZaposlenik.Lozinka),
+                    Spol = noviZaposlenik.Spol,
+                    Email=noviZaposlenik.Email,
+                    Broj_mobitela=noviZaposlenik.Broj_mobitela,
+                    OIB=noviZaposlenik.OIB,
+                    Datum_zaposlenja=noviZaposlenik.Datum_zaposlenja,
+                    Id_VrstaZaposlenika=noviZaposlenik.Id_VrstaZaposlenika
+                    };
+                baza.Zaposlenik.Add(zaposlenik);
+                baza.SaveChanges();
+            }
+
+
+        }
+
+        public string GenerirajHash(string lozinka)
+        {
+            
+            if (string.IsNullOrEmpty(lozinka))
+                   return string.Empty;
+
+                using (var sha = new System.Security.Cryptography.SHA256Managed())
+                {
+                    byte[] tekst = Encoding.UTF8.GetBytes(lozinka);
+                    byte[] hash = sha.ComputeHash(tekst);
+                    return BitConverter.ToString(hash).Replace("-", string.Empty);
+                }
+            
+        }
+    }
+}
